feb489495107ef3e580468ddbc2b1f47e9c65360
yunioseijo <yunioseijo@gmail.com>
2023-08-02 12:25:09 +0200

    first commit to http json

---
+++ app/app.js
@@ -0,0 +1,56 @@
+// como voy a usar la lib route tengo que pasarla como parametro
+var myNinjaApp = angular.module('myNinjaApp', ['ngRoute']);
+
+// creo el método config usando el objeto routeProvider
+myNinjaApp.config(['$routeProvider', function($routeProvider) {
+  
+  $routeProvider
+    .when('/home',{templateUrl: 'app/views/home.html'})
+    .when('/directory',{templateUrl: 'app/views/directory.html', controller: 'NinjaController'})
+    .otherwise({redirectTo: '/home'});
+  }]);
+
+myNinjaApp.controller('NinjaController',['$scope', '$http', function($scope, $http){
+  $scope.removeNinja = function(ninja){
+    
+    // var removeNinjaIndex = $scope.ninjas.indexOf(ninja);
+    // $scope.ninjas.splice(removeNinjaIndex, 1);
+    $scope.ninjas = $scope.ninjas.filter(function(item) {
+      return item !== ninja;
+  }); 
+  }
+
+  $scope.addNinja=function(){
+    $scope.ninjas.push({
+      name: $scope.newninja.name,
+      belt: $scope.newninja.belt,
+      rate: parseInt($scope.newninja.rate),
+      available: true
+    });
+    $scope.newninja.name="";
+    $scope.newninja.belt="";
+    $scope.newninja.rate="";
+  };
+
+
+  // estos datos los voy a cargar desde un fichero externo  como un JSON, para ello necesito usar 
+  // el servicio de http de angular y pasarselo como parámetro al controlador
+  // $scope.ninjas = [
+  //   {name: 'Yoshi',belt: 'green', rate:50,available:true,thumb: 'content/image/cat-ninja.webp'},
+  //   {name: 'Crystal',belt: 'yellow', rate:30,available:true,thumb: 'content/image/ninja_octopus.webp'},
+  //   {name: 'Ryu',belt: 'orange', rate:70,available:false,thumb: 'content/image/ninja_technology.webp'},
+  //   {name: 'Shaun',belt: 'black', rate:100,available:true,thumb: 'content/image/sloth-ninja-hang.webp'}
+  // ];
+
+  // console.log(angular.toJson($scope.ninjas));
+
+  $http.get('data/ninjas.json')
+  .then(function(response) {
+    $scope.ninjas = response.data;
+  })
+  .catch(function(error) {
+    // Manejar el error en caso de que ocurra
+    console.error('Error al obtener los datos:', error);
+  });
+
+}]);
